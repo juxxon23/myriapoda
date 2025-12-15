ddd35c2fb92534ecb731daa73b52646fe84a73c9
juxxon23 <juxxon23@hotmail.com>
2025-12-15 16:40:18 -0500

    Add dash movement

--- gameplay/gameplay.gd
+++ gameplay/gameplay.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 const gameover_scene:  PackedScene = preload("uid://mdfrrnsaukcp")
 const pausemenu_scene:  PackedScene = preload("uid://bpankxymenwob")
+const BASIC_SPEED: float = 5000.0 
 
 @onready var head: Head = %Head as Head # 'as Head' helps with the autocomplete of Head's functions
 @onready var bounds: Bounds = %Bounds as Bounds
@@ -10,10 +11,12 @@ const pausemenu_scene:  PackedScene = preload("uid://bpankxymenwob")
 @onready var hud: HUD = %HUD as HUD
 @onready var segment: Segment = %Segment as Segment
 @onready var tail: Area2D = %Tail as Tail
+@onready var dash_timer: Timer = %DashTimer as Timer
 
 var time_between_moves: float = 1000.0
 var time_since_last_move: float = 0
-var speed: float = 5000.0
+var speed: float = BASIC_SPEED
+var speed_accumulation: float = 0.0
 var move_dir: Vector2 = Vector2.RIGHT
 var snake_parts: Array[SnakePart] = []
 var gameover_menu: GameOver
@@ -55,9 +58,14 @@ func _process(_delta: float) -> void:
 		
 	if Input.is_action_just_pressed("ui_cancel"):
 		pause_game()
+		
+	if Input.is_action_just_pressed("ui_dash"):
+		dash_timer.start()
+		speed = speed * 5
 
 
 func _physics_process(delta: float) -> void:
+	print(speed, " @@@ ", speed_accumulation)
 	time_since_last_move += delta * speed
 	if time_since_last_move >= time_between_moves:
 		update_snake()
@@ -78,6 +86,7 @@ func update_snake() -> void:
 func _on_food_eaten() -> void:
 	spawner.call_deferred("spawn_food")
 	spawner.call_deferred("spawn_segment", snake_parts[snake_parts.size()-2].penultimate_position)
+	speed_accumulation += 500.0
 	speed += 500.0
 	score += 1
 
@@ -97,7 +106,12 @@ func _notification(what: int) -> void:
 	if what == NOTIFICATION_WM_WINDOW_FOCUS_OUT:
 		pause_game()
 
+
 func pause_game() -> void:
 	if not pause_menu:
 		pause_menu = pausemenu_scene.instantiate() as PauseMenu
 		add_child(pause_menu)
+
+
+func _on_dash_timer_timeout() -> void:
+	speed = BASIC_SPEED + speed_accumulation
